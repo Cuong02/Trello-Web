0ee3381f02742d9acd6c8588678462f5079aac16
Quang Cường <thanlequangcuong@gmail.com>
2024-04-29 01:19:25 +0700

    feat: get board

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -7,6 +7,13 @@ const createNew = async (req, res, next) => {
     res.status(StatusCodes.CREATED).json(await boardService.createNew(req.body))
   } catch (error) {next(error)}
 }
+const getDetails = async (req, res, next) => {
+  try {
+    const boardId = req.params.id
+    res.status(StatusCodes.OK).json(await boardService.getDetails(boardId))
+  } catch (error) {next(error)}
+}
 export const boardController = {
-  createNew
+  createNew,
+  getDetails
 }
