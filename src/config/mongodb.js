01400a348c6afd716e10426525f1e89461d9b1ce
Quang Cường <thanlequangcuong@gmail.com>
2024-04-29 17:10:09 +0700

    feat: drag&drop columns

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -2,22 +2,30 @@
 
 import { env } from './environment'
 import { MongoClient, ServerApiVersion } from 'mongodb'
-let trelloDatabaseInstance = null
 
+// Khởi tạo 1 đối tượng trelloDatabaseInstance ban đầu là null
+let trelloDatabaseInstance = null
+// Khởi tạo một đối tượng Client Instance để connect tới MongoDB
 const mongoClientInstance = new MongoClient(env.MONGODB_URI, {
   serverApi : {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true }
 })
+
+// Đóng kết nối tới Database khi cần
 export const CLOSE_DB = async() => {
   await mongoClientInstance.close()
 }
+
 // Ket noi toi MongoDB
 export const CONNECT_DB = async() => {
+  // Gọi kết nối tới MongoDB Atlas với URI đã khai báo trong thân của mongoClientInstance
   await mongoClientInstance.connect()
-  trelloDatabaseInstance= mongoClientInstance.db(env.DB_NAME)
+  // Kết nối thành công thì lấy ra Database theo tên và gán ngược lại vào biến trelloDatabaseInstance ở trên
+  trelloDatabaseInstance= mongoClientInstance.db(env.DATABASE_NAME)
 }
+
 // Sau khi connect thanh cong toi MongoDb thi su dung ham GET_DB export nhieu noi trong code
 export const GET_DB = () => {
   if (!trelloDatabaseInstance) throw new Error('Must connect to DB first')
--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,19 +1,45 @@
+// import express from 'express'
 import { StatusCodes } from 'http-status-codes'
 import { boardService } from '~/services/boardService'
+
 const createNew = async (req, res, next) => {
+
   try {
-    // Dieu huong du lieu sang tang Service
-    // Co ket qua tra ve phia client
-    res.status(StatusCodes.CREATED).json(await boardService.createNew(req.body))
-  } catch (error) {next(error)}
+
+    // Điều hướng dữ liệu sang tầng Service
+    const createdBoard = await boardService.createNew(req.body)
+
+    // Có kết quả trả  về phía Client
+    res.status(StatusCodes.CREATED).json(createdBoard)
+  } catch (error) { next(error) }
 }
+
 const getDetails = async (req, res, next) => {
   try {
     const boardId = req.params.id
-    res.status(StatusCodes.OK).json(await boardService.getDetails(boardId))
-  } catch (error) {next(error)}
+    const board = await boardService.getDetails(boardId)
+    res.status(StatusCodes.OK).json(board)
+  } catch (error) { next(error) }
 }
+
+const update = async (req, res, next) => {
+  try {
+    const boardId = req.params.id
+    const updatedBoard = await boardService.update(boardId, req.body)
+    res.status(StatusCodes.OK).json(updatedBoard)
+  } catch (error) { next(error) }
+}
+
+const moveCardToDifferentColumn = async (req, res, next) => {
+  try {
+    const result = await boardService.moveCardToDifferentColumn(req.body)
+    res.status(StatusCodes.OK).json(result)
+  } catch (error) { next(error) }
+}
+
 export const boardController = {
   createNew,
-  getDetails
+  getDetails,
+  update,
+  moveCardToDifferentColumn
 }
