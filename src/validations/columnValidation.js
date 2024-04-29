8030b7215e7345b4137558e1bc9dd6d15cb907a0
Quang Cường <thanlequangcuong@gmail.com>
2024-04-29 15:36:05 +0700

    feat: add new column&card

---
+++ src/validations/columnValidation.js
@@ -0,0 +1,49 @@
+
+import Joi from 'joi'
+import { StatusCodes } from 'http-status-codes'
+import ApiError from '~/utils/ApiError'
+import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
+
+const createNew = async (req, res, next) => {
+  const correctCondition = Joi.object({
+    boardId: Joi.string().required().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE),
+    title: Joi.string().required().min(3).max(50).trim().strict()
+  })
+
+  try {
+    await correctCondition.validateAsync(req.body, { abortEarly: false })
+    next()
+  } catch (error) {
+    next(new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, new Error(error).message))
+  }
+}
+
+const update = async (req, res, next) => {
+  // Lưu ý: không dùng hàm required() trong trường hợp update
+  const correctCondition = Joi.object({
+    // Nếu cần làm tiin1h năng di chuyển column sang Board khác thì cần validate boardId
+    // boardId: Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE),
+    title: Joi.string().min(3).max(50).trim().strict(),
+    columnOrderIds: Joi.array().items(
+      Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)
+    )
+  })
+
+  try {
+    // set abortEarly: false để trường hợp có nhiều lỗi validation thì trả về tất cả lỗi
+    await correctCondition.validateAsync(req.body, {
+      abortEarly: false,
+      allowUnknown: true // cho phép validate các key không được định nghĩa trong schema
+    }) //json object 'abortEarly: false': gặp lỗi có dừng luôn hay không?
+    next()
+  } catch (error) {
+    // next một cái ApiError(StatusCodes, message)
+    next(new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, new Error(error).message))
+  }
+}
+
+export const columnValidation = {
+  createNew,
+  update
+}
+
