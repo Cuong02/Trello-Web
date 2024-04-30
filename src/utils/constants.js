d5418133f53ec5a37dfbd7727b98d14bafc1f595
Quang Cường <thanlequangcuong@gmail.com>
2024-05-01 01:40:36 +0700

    fix: link domain

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,8 @@
 
 // Những domain được phép truy cập tới tài nguyen của Server
 export const WHITELIST_DOMAINS = [
-  // 'http://localhost:5173'
+  'https://trello-web-tan.vercel.app'
+  //'http://localhost:5173'
   // sau này sẽ deploy lên domain chính thức
 ]
 
